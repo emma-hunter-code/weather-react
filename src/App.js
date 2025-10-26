4ef72183e9325981b72b1ecce396baa38df0ce84
Emma Hunter <mmrbrts86@gmail.com>
2025-10-26 10:50:05 +0000

    Added rel="noreferrer" to fix deploy failure

--- src/App.js
+++ src/App.js
@@ -11,11 +11,16 @@ export default function App() {
         <a
           href="https://github.com/emma-hunter-code/weather-react"
           target="_blank"
+          rel="noreferrer"
         >
           GitHub
         </a>
         and hosted on{" "}
-        <a href="https://weathreact.netlify.app/" target="_blank">
+        <a
+          href="https://weathreact.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Netlify
         </a>
         .
