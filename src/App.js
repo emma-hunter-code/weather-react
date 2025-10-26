3d94eff35f997d1faacdee29c31cb95d720271cf
Emma Hunter <mmrbrts86@gmail.com>
2025-10-26 11:24:45 +0000

    Added bootstrap container div to App.js

--- src/App.js
+++ src/App.js
@@ -4,27 +4,29 @@ import Search from "./Search";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search</h1>
-      <Search />
-      <footer>
-        Coded by Emma Hunter on{" "}
-        <a
-          href="https://github.com/emma-hunter-code/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://weathreact.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather Search</h1>
+        <Search />
+        <footer>
+          Coded by Emma Hunter on{" "}
+          <a
+            href="https://github.com/emma-hunter-code/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://weathreact.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
