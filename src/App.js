b1266e7e41bda0dcd05ebce6ace9ee21a5857750
Emma Hunter <mmrbrts86@gmail.com>
2025-10-26 10:47:19 +0000

    Added signature with github and netlify links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ export default function App() {
     <div className="App">
       <h1>Weather Search</h1>
       <Search />
+      <p>
+        Coded by Emma Hunter on{" "}
+        <a
+          href="https://github.com/emma-hunter-code/weather-react"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        and hosted on{" "}
+        <a href="https://weathreact.netlify.app/" target="_blank">
+          Netlify
+        </a>
+        .
+      </p>
     </div>
   );
 }
