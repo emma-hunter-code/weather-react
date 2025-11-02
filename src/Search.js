e1dfba4fbd9a4e3b72585be494494f1c2c2f04e5
Emma Hunter <mmrbrts86@gmail.com>
2025-11-02 14:58:01 +0000

    Added WeatherInfo component and updated css

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Search.css";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Search() {
   const [city, setCity] = useState("");
@@ -56,44 +56,12 @@ export default function Search() {
 
   if (loaded) {
     return (
-      <div className="Search">
+      <div>
         {form}
-        <h1>{weather.name}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weather.date} />
-          </li>
-          <li className="text-capitalize">
-            Description: {weather.description}
-          </li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <div className="clearfix">
-              <img
-                src={weather.icon}
-                alt={weather.description}
-                className="float-left"
-              />
-              <div className="float-left">
-                <span className="temperature">
-                  {Math.round(weather.temperature)}
-                </span>
-                <span className="unit">°C</span>
-              </div>
-            </div>
-          </div>
-          <div className="col-6">
-            {" "}
-            <ul>
-              <li>Humidity: {weather.humidity}%</li>
-              <li>Wind: {weather.wind}km/h</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weather} />
       </div>
     );
   } else {
-    return form;
+    return <div>{form}</div>;
   }
 }
